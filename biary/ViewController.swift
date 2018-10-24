0f66a22d9fbe5d51d05deac75e2d47197da3140b
changhyeon743 <changhyeon743@gmail.com>
2018-10-25 08:34:54 +0900

    initial commit

---
+++ biary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  biary
+//
+//  Created by 이창현 on 25/10/2018.
+//  Copyright © 2018 이창현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
