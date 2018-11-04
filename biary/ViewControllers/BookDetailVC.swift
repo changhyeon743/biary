4527a591d77b3ccc0530d503499f702aac0b9d1d
changhyeon743 <changhyeon743@gmail.com>
2018-11-04 20:03:38 +0900

    commit

--- biary/ViewControllers/BookDetailVC.swift
+++ biary/ViewControllers/BookDetailVC.swift
@@ -8,96 +8,82 @@
 
 import UIKit
 
-class BookDetailVC: UIViewController {
+class BookDetailVC: UITableViewController {
 
-    @IBOutlet weak var tableView: UITableView!
+    var headerView:UIView!
+    var newHeaderLayer: CAShapeLayer!
     
-    var headerView:BookDetailHeaderView!
-    var headerHeightConstraint:NSLayoutConstraint!
-    let headerHeight:CGFloat = 250
+    private let headerHeight: CGFloat = 250
+    private let headerCut: CGFloat = 0
+    
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
     override func viewDidLoad() {
-        setUpHeader()
-        setUpTableView()
+        self.updateView()
     }
     
-    func setUpTableView() {
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        let constraints:[NSLayoutConstraint] = [
-            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ]
-        NSLayoutConstraint.activate(constraints)
+    func updateView() {
+        tableView.backgroundColor = UIColor.white
+        headerView = tableView.tableHeaderView
+        tableView.tableHeaderView = nil
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.addSubview(headerView)
+        
+        newHeaderLayer = CAShapeLayer()
+        newHeaderLayer.fillColor = UIColor.black.cgColor
+        headerView.layer.mask = newHeaderLayer
+        
+        let newheight = headerHeight - headerCut / 2
+        tableView.contentInset = UIEdgeInsets(top: newheight, left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -newheight)
+        
+        self.setUpNewView()
+    }
+    
+    func setUpNewView() {
+        let newheight = headerHeight - headerCut / 2
+        var getheaderframe = CGRect(x: 0, y: -newheight, width: tableView.bounds.width, height: headerHeight)
+        if tableView.contentOffset.y < newheight
+        {
+            getheaderframe.origin.y = tableView.contentOffset.y
+            getheaderframe.size.height = -tableView.contentOffset.y + headerCut / 2
+        }
+        
+        headerView.frame = getheaderframe
+        let cutdirection = UIBezierPath()
+        cutdirection.move(to: CGPoint(x: 0, y: 0))
+        cutdirection.addLine(to: CGPoint(x: getheaderframe.width, y: 0))
+        cutdirection.addLine(to: CGPoint(x: getheaderframe.width, y: getheaderframe.height))
+        cutdirection.addLine(to: CGPoint(x: 0, y: getheaderframe.height - headerCut))
+        newHeaderLayer.path = cutdirection.cgPath
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        tableView.decelerationRate = UIScrollView.DecelerationRate.fast
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        setUpNewView()
     }
     
     
-    func setUpHeader() {
-        headerView = BookDetailHeaderView(frame: CGRect.zero, title: "Articles")
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(headerView)
-        headerHeightConstraint = headerView.heightAnchor.constraint(equalToConstant: headerHeight)
-        headerHeightConstraint.isActive = true
-        //top, left, right, and height on 150
-        let constraints:[NSLayoutConstraint] = [
-            headerView.topAnchor.constraint(equalTo: view.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ]
-        NSLayoutConstraint.activate(constraints)
-    }
-
 }
 
-extension BookDetailVC:UITableViewDelegate,UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+extension BookDetailVC {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = "텍스트"
-        return cell!
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "텍스트"
+        return cell
     }
-    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
     
 }
-
-extension BookDetailVC:UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.contentOffset.y < 0 {
-            self.headerHeightConstraint.constant += abs(scrollView.contentOffset.y)/2
-        } else if scrollView.contentOffset.y > 0 && headerHeightConstraint.constant > 0 {
-            self.headerHeightConstraint.constant -= abs(scrollView.contentOffset.y)
-        }
-    }
-    
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if self.headerHeightConstraint.constant > headerHeight {
-            animateHeader()
-        }
-    }
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        if self.headerHeightConstraint.constant > headerHeight {
-            animateHeader()
-        }
-    }
-    
-    
-    func animateHeader() {
-        
-        self.headerHeightConstraint.constant = headerHeight
-        
-        UIView.animate(withDuration: 0.3, delay: 0.0, options: .curveEaseInOut, animations: {
-            self.view.layoutIfNeeded()
-        }, completion: nil)
-        
-    }
-}
-
-
